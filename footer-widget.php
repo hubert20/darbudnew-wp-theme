f1fed1f893dcf06fd6760df4e15a9177b1b6b6b7
Hubert <hubert@tomedia.pl>
2025-09-27 19:16:26 +0200

    New files 4

--- footer-widget.php
+++ footer-widget.php
@@ -3,7 +3,7 @@
     <div class="container py-4 py-lg-5">
         <div class="row justify-content-center">
             <div class="col-8 col-lg-3 text-center">
-                <img src="https://mariolahairstylist.pl/wp-content/uploads/2025/06/flowers-200.png" class="img-fluid mb-4 mb-lg-5 mx-auto">
+                <img src="" class="img-fluid mb-4 mb-lg-5 mx-auto">
             </div>
         </div>
         <div class="footer-widgets__logo mb-4 mb-lg-5"></div>
@@ -29,7 +29,7 @@
 </section>
 <div class="copyright-box">
     <div class="container py-4">
-        <p class="text-center text-gray-light mb-0"><small>mariolahairstylist Copyright 2025</small></p>
+        <p class="text-center text-gray-light mb-0"><small>darbud Copyright 2025</small></p>
     </div>
 </div>
 
