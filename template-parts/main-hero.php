01f2efb7a4620bdb5ea8c8d172f247070ad009fc
Hubert <hubert@tomedia.pl>
2025-09-29 18:17:50 +0200

    Added bottom elements v.2

--- template-parts/main-hero.php
+++ template-parts/main-hero.php
@@ -3,7 +3,7 @@ $banner_cnt = get_field('banner_cnt');
 $banner_grafika = get_field('banner_grafika');
 ?>
 
-<div class="px-lg-5 py-4 py-lg-5">
+<div class="px-lg-5 pb-4 pb-lg-5 hero-main">
     <div class="container-fluid text-center hero_wrap position-relative" style="background-image: url('<?php echo $banner_grafika; ?>')">
         <?php echo $banner_cnt; ?>
     </div>
