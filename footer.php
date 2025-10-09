755c7160cd5e6071897615f07b63f9a5d7255b2d
Hubert <hubert@tomedia.pl>
2025-10-09 23:22:26 +0200

    form v.2

--- footer.php
+++ footer.php
@@ -36,23 +36,25 @@
 </div>
 
 <!-- Offer form Modal -->
-<div class="modal fade" id="offerformModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="offerformModal" tabindex="-1" aria-labelledby="offerformModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header bg-blue-dark d-flex py-1 justify-content-between">
-            <img src="/wp-content/uploads/2023/03/logo_white-1.png" alt="darbud.com.pl" class="img-fluid" style="height: 40px;">
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true"></span>
-                </button>
+
+            <div class="modal-header bg-primary py-2">
+                <img src="/wp-content/uploads/2023/03/logo_white-1.png" alt="darbud.com.pl" class="img-fluid" style="height: 40px;">
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+
             <div class="modal-body" id="modal-form">
                 <div class="modal-form-cnt" data-category-type="pomoc-organizacji">
                     <?php echo apply_shortcodes('[contact-form-7 id="3524678" title="Formularz kontaktowy"]'); ?>
                 </div>
             </div>
+
         </div>
     </div>
 </div>
+
 
 <footer class="site-footer" role="contentinfo">
     <?php get_template_part('footer-widget'); ?>
