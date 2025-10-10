02fa15d819e93362604ceff8f97eee6626605582
Hubert <hubert@tomedia.pl>
2025-10-10 23:55:41 +0200

    form v.7

--- template-parts/main-form.php
+++ template-parts/main-form.php
@@ -6,7 +6,7 @@ $formularz_cnt = get_field('formularz_cnt');
 <section class="main-form py-4 py-lg-5" style="background-image: url('<?php echo $bg_main_form; ?>')">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 offset-lg-6">
+            <div class="col-lg-5 offset-lg-8">
                 <?php echo apply_shortcodes('[contact-form-7 id="3524678" title="Formularz kontaktowy"]'); ?>
             </div>
         </div>
