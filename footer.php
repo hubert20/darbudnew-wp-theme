7cbaa833aa3594e8da14e8fb8008628ed1570ef4
Hubert <hubert@tomedia.pl>
2025-10-12 12:27:05 +0200

    Added modal with video YT

--- footer.php
+++ footer.php
@@ -14,18 +14,16 @@
 <div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="videoModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header bg-primary py-2">
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
             <div class="modal-body" id="modal-form">
-                <div class="modal-form-cnt">
-                    <?php echo apply_shortcodes('[contact-form-7 id="3524678" title="Formularz kontaktowy"]'); ?>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                <!-- 16:9 aspect ratio -->
+                <div class="ratio ratio-16x9">
+                    <iframe class="embed-responsive-item" src="" id="video" allowscriptaccess="always" allow="autoplay"></iframe>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
 
 <footer class="site-footer" role="contentinfo">
     <?php get_template_part('footer-widget'); ?>
