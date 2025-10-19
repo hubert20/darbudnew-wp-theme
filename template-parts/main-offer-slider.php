d8016387f572d9e319d410817866ffa66a86471c
Hubert <hubert@tomedia.pl>
2025-10-19 17:24:19 +0200

    Swiper on sg products background v.25

--- template-parts/main-offer-slider.php
+++ template-parts/main-offer-slider.php
@@ -66,11 +66,6 @@
                                 // Jeśli nie ma żadnego obrazka — nic nie renderujemy, ale kategoria i tak zostaje pokazana (tytuł/opis poniżej)
                                 ?>
                                 <div class="main-products--item-title p-3">
-                                    <span class="main-products--item-corner">
-                                        <svg class="arrow-icon" width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M0.38296 20.0762C0.111788 19.805 0.111788 19.3654 0.38296 19.0942L9.19758 10.2796L0.38296 1.46497C0.111788 1.19379 0.111788 0.754138 0.38296 0.482966C0.654131 0.211794 1.09379 0.211794 1.36496 0.482966L10.4341 9.55214C10.8359 9.9539 10.8359 10.6053 10.4341 11.007L1.36496 20.0762C1.09379 20.3474 0.654131 20.3474 0.38296 20.0762Z" fill="currentColor" />
-                                        </svg>
-                                    </span>
                                     <?php if (!empty($category->name)) : ?>
                                         <p class="mb-0 text-green text0uppercase standard-title-6 rajdhani-600"><?php echo esc_html($category->name); ?></p>
                                     <?php endif; ?>
@@ -79,6 +74,12 @@
                                             <?php echo esc_html($category->description); ?>
                                         </p>
                                     <?php endif; ?>
+                                    <!-- Arrow -->
+                                    <span class="main-products--item-corner">
+                                        <svg class="arrow-icon" width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M0.38296 20.0762C0.111788 19.805 0.111788 19.3654 0.38296 19.0942L9.19758 10.2796L0.38296 1.46497C0.111788 1.19379 0.111788 0.754138 0.38296 0.482966C0.654131 0.211794 1.09379 0.211794 1.36496 0.482966L10.4341 9.55214C10.8359 9.9539 10.8359 10.6053 10.4341 11.007L1.36496 20.0762C1.09379 20.3474 0.654131 20.3474 0.38296 20.0762Z" fill="currentColor" />
+                                        </svg>
+                                    </span>
                                 </div>
                             </a>
                         </div>
